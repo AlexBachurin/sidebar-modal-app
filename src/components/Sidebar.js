962d2642f771688b2deafe94132bfef5aeba70a8
Alex <alexbachurin91@gmail.com>
2021-09-15 04:25:33 +0300

    dynamically show links and social-icons from data

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { AiOutlineClose } from 'react-icons/ai';
+import { links, social } from '../data'
 const Sidebar = () => {
     return (
         <aside className="sidebar show-sidebar">
             <div className="sidebar-header">
-                <img src="" alt="logo" className="logo" />
+                <img src="https://res.cloudinary.com/dljezd6qv/image/upload/v1631669002/logo-horizontal-svg_jg4e3x.svg" alt="logo" className="logo" />
                 <button className="close-btn">
                     <AiOutlineClose />
                 </button>
@@ -12,11 +13,25 @@ const Sidebar = () => {
 
             </div>
             <ul className="links">
-                <li><a href="#">home</a></li>
-                <li><a href="#">etc</a></li>
+                {/* display links from data */}
+                {links.map(({ id, url, text, icon }) => {
+                    return (
+                        <li key={id}>
+                            <a href={url}>{icon}{text}</a>
+                        </li>
+                    )
+                })}
+
             </ul>
             <ul className="social-icons">
-                <li><a href="#">twitter</a></li>
+                {/* display social icons from data */}
+                {social.map(({ id, url, icon }) => {
+                    return (
+                        <li key={id}>
+                            <a href={url}>{icon}</a>
+                        </li>
+                    )
+                })}
 
             </ul>
         </aside>
